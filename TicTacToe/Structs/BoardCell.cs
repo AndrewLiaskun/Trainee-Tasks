9acbacb7c4e725fe97f83ed8201cf3b7a20c0c3c
Andrew <hiigarawolf@gmail.com>
2021-06-21 14:53:18 +0300

    TicTacToe v0.2

---
+++ TicTacToe/Structs/BoardCell.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2021 Medtronic, Inc. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class BoardCell
+    {
+        public const char DefaultCharValue = ' ';
+        public const char ZeroChar = 'O';
+        public const char CrossChar = 'X';
+
+        public BoardCell(Point point, char value = DefaultCharValue)
+        {
+            Point = point;
+            Value = value;
+        }
+
+        public Point Point { get; }
+
+        public char Value { get; set; }
+    }
+}
---
+++ TicTacToe/Structs/Point.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2021 Medtronic, Inc. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public struct Point
+    {
+        public Point(int x = 0, int y = 0)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int _x { get; set; }
+
+        public int _y { get; set; }
+
+        public override string ToString() => $"Point:({_x},{_y})";
+    }
+}
