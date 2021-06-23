5600fe90d9d7ab3022f0fc2e23aea9ac34b6973b
Andrew <hiigarawolf@gmail.com>
2021-06-23 16:57:44 +0300

    TicTacToe v 0.4

---
+++ TicTacToe/Structs/KBDLLHOOKSTRUCT.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2021 Medtronic, Inc. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Structs
+{
+    public struct KBDLLHOOKSTRUCT
+    {
+        public int vkCode;
+        public int flags;
+    }
+}
