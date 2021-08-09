4866564186acb63a6dd42ff42b69850e521bb843
Andrew <hiigarawolf@gmail.com>
2021-08-09 13:39:09 +0300

    [+] Create Battleships.UI project with folders and base classes.

---
+++ BattleShips.UI/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2021 Medtronic, Inc. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BattleShips.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
